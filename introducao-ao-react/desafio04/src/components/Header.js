dbd453b862531ec00fc09421692eeefbe0aaa084
RenanArantes <renan.arantes@rede.ulbra.br>
2020-02-18 23:47:19 -0300

    initial-commit-basic-confid-and-structures

---
+++ introducao-ao-react/desafio04/src/components/Header.js
@@ -0,0 +1,24 @@
+import React,{ Component } from 'react';
+
+import logo from '../assets/logo.png';
+import avatar from '../assets/avatar.png';
+
+class Header extends Component {
+    state = {}
+
+    render() {
+        return (
+            <header>
+                <nav>
+                    <img src={logo} alt="Facebook Logo"></img>
+                    <div>
+                        <p>Usuario</p>
+                        <i class="fas fa-user-circle"></i>
+                    </div>
+                </nav>
+            </header>
+        )
+    }
+}
+
+export default Header;
