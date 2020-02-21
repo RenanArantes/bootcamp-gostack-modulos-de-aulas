71d21d9ef0807481eb57cb0fba2dc0bb1406abf9
Chuckrutis <renan.arantes@rede.ulbra.br>
2020-02-21 15:51:02 -0300

    Adicionando repositorios
    feito ate o minuto 9:40

--- primeiro-projeto-com-react/aulas/modulo05/src/pages/Main/index.js
+++ primeiro-projeto-com-react/aulas/modulo05/src/pages/Main/index.js
@@ -1,12 +1,67 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { FaGithubAlt, FaPlus } from 'react-icons/fa';
 
-import { Title } from './styles';
+import api from '../../services/api';
 
-export default function Main() {
-  return (
-    <Title>
-      Main
-    </Title>
-  )
+import { Container, Form, SubmitButton } from './styles';
+
+export default class Main extends Component {
+  state = {
+    newRepo: '',
+    repositories: [],
+    loading: false,
+  }
+
+  handleInputChange = e => {
+    this.setState({ newRepo: e.target.value });
+  }
+
+  handleSubmit = async e => {
+    e.preventDefault();
+
+    this.setState({ loading: true });
+
+    const { newRepo, repositories } = this.state;
+
+    const response = await api.get(`/repos/${newRepo}`);
+
+    const data = {
+      name: response.data.full_name,
+    }
+
+    this.setState({
+      repositories: [...repositories, data],
+      newRepo: '',
+      loading: false,
+    })
+
+    console.log(response.data);
+  }
+
+  render() {
+    const { newRepo, loading } = this.state;
+
+    return (
+      <Container>
+        <h1>
+          <FaGithubAlt />
+          Repositórios
+        </h1>
+  
+        <Form onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            placeholder="Adicionar repositório"
+            value={newRepo}
+            onChange={this.handleInputChange}
+          />
+
+          <SubmitButton loading={loading}>
+            <FaPlus color="#FFF" size={14} />
+          </SubmitButton>
+        </Form>
+      </Container>
+    )
+  }
     
 }
