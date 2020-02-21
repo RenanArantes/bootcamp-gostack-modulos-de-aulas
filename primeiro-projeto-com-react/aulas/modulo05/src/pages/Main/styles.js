d5cb4627bcb4d427f942d9695482e15eda9eb576
Chuckrutis <renan.arantes@rede.ulbra.br>
2020-02-21 10:58:18 -0300

    styled

--- primeiro-projeto-com-react/aulas/modulo05/src/pages/Main/styles.js
+++ primeiro-projeto-com-react/aulas/modulo05/src/pages/Main/styles.js
@@ -1,8 +1,5 @@
 import styled from 'styled-components';
 
 export const Title = styled.h1 `
-    font-size: 24px;
-    color: #7159c1;
-    font-family: Arial, Helvetica, sans-seirf;
-
+    color: #FFF
 `;
