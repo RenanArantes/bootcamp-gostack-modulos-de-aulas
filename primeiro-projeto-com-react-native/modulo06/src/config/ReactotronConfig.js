1db14cfe4eee1178b2cb39971eca57001b6c66a5
RenanArantes <renan.arantes@rede.ulbra.br>
2020-02-24 20:15:14 -0300

    react-nagivation-structure

--- primeiro-projeto-com-react-native/modulo06/src/config/ReactotronConfig.js
+++ primeiro-projeto-com-react-native/modulo06/src/config/ReactotronConfig.js
@@ -1,12 +1,9 @@
 import Reactotron from 'reactotron-react-native';
-import AsyncStorage from '@react-native-community/async-storage';
 
 if (__DEV__) {
-    const tron = Reactotron.configure({ host: '192.168.1.12' })
+    const tron = Reactotron.configure({ host: '192.168.0.108' })
         .useReactNative()
         .connect();
 
     console.tron = tron;
-
-    tron.clear();
 }
