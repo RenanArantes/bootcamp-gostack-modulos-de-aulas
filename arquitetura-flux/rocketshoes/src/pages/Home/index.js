1a7a658395706d85d41bca9be1f44ee5dd105bfd
RenanArantes <renan.arantes@rede.ulbra.br>
2020-03-17 22:40:01 -0300

    redux fluxo

--- arquitetura-flux/rocketshoes/src/pages/Home/index.js
+++ arquitetura-flux/rocketshoes/src/pages/Home/index.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import { MdAddShoppingCart } from 'react-icons/md';
+import { connect } from 'react-redux';
+import { formatPrice } from '../../utils/format';
 import api from '../../services/api';
 
 import { ProductList } from './styles';
 
-export default class Home extends Component {
+class Home extends Component {
     state = {
         products: [],
     };
@@ -12,8 +14,22 @@ export default class Home extends Component {
     async componentDidMount() {
         const response = await api.get('products');
 
-        this.setState({ products: response.data });
+        const data = response.data.map(product => ({
+            ...product,
+            priceFormated: formatPrice(product.price),
+        }));
+
+        this.setState({ products: data });
     }
+
+    handleAddProduct = product => {
+        const { dispatch } = this.props;
+
+        dispatch({
+            type: 'ADD_TO_CART',
+            product,
+        });
+    };
 
     render() {
         const { products } = this.state;
@@ -24,13 +40,14 @@ export default class Home extends Component {
                     <li key={product.id}>
                         <img src={product.image} alt={product.title} />
                         <strong>{product.title}</strong>
-                        <span>{product.price}</span>
-
-                        <button type="button">
+                        <span>{product.priceFormated}</span>
+                        <button
+                            type="button"
+                            onClick={() => this.handleAddProduct(product)}
+                        >
                             <div>
                                 <MdAddShoppingCart size={16} color="#FFF" /> 3
                             </div>
-
                             <span>Adicionar ao carrinho</span>
                         </button>
                     </li>
@@ -39,3 +56,5 @@ export default class Home extends Component {
         );
     }
 }
+
+export default connect()(Home);
