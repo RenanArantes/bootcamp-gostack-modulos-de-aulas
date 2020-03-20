ff2a1e041c08723c8b3c10934f27ceb3c0233c40
RenanArantes <renan.arantes@rede.ulbra.br>
2020-03-19 22:56:01 -0300

    Avisos de estoque toastify, reactotron + saga e history nagivation

--- arquitetura-flux/rocketshoes/src/store/modules/rootSaga.js
+++ arquitetura-flux/rocketshoes/src/store/modules/rootSaga.js
@@ -1,6 +1,6 @@
 import { all } from 'redux-saga/effects';
 
-import cart from './cart/sagas';
+import cart from './cart/saga';
 
 export default function* rootSaga() {
     return yield all([cart]);
